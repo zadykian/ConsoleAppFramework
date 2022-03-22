a30be2138ae64b7de71ad4954cc45c6a3c9dea47
zadykian <zadykian@outlook.com>
2022-03-22 12:42:52 +0300

    PublishTrimmedAppTest

--- tests/ConsoleAppFramework.Tests/Integration/PublishTrimmedAppTest.cs
+++ tests/ConsoleAppFramework.Tests/Integration/PublishTrimmedAppTest.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using CliWrap;
 using CliWrap.Buffered;
@@ -21,14 +22,17 @@ public class PublishTrimmedAppTest
 	[Fact]
 	public async Task Publish_Trimmed_Assembly()
 	{
+		var output = new StringBuilder();
+		
 		var result = await Cli
 			.Wrap("dotnet")
-			.WithArguments("publish tests/ConsoleAppFramework.Test.TrimmedAssembly --runtime win-x64")
+			.WithArguments("publish tests/ConsoleAppFramework.Test.TrimmedAssembly --self-contained --runtime win-x64")
 			.WithValidation(CommandResultValidation.None)
 			.WithWorkingDirectory(GetSolutionDirectoryPath())
+			.WithStandardOutputPipe(PipeTarget.ToStringBuilder(output))
 			.ExecuteBufferedAsync();
 
-		result.ExitCode.Should().Be(0, "publish command should complete successfully");
+		result.ExitCode.Should().Be(0, $"Publish command should complete successfully. Output: [{output}]");
 		result.StandardError.Should().BeEmpty();
 	}
 }
